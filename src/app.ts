7774712b89fc6056312c9445a20043f1fd7e4ec7
josereyna07 <josedaniel3046@gmail.com>
2025-02-17 14:40:12 -0500

    metodo crear y seleccionar

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
-import express, { Express, Request } from 'express'
-import cors from 'cors'
+import express, { Express, Request } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
 
-app.set('port', 4000)
-app.use(cors<Request>())
-app.use(express.static('./src/view'))
-app.use(express.json())
+let users: string[] = [];
 
-app.listen(app.get('port'), () => console.log(`Server running on: http://localhost:${app.get('port')}`))
+app.set('port', 4000);
+app.use(cors<Request>());
+app.use(express.static('./src/view'));
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const { username } = req.body;
+  if (username && !users.includes(username)) {
+    users.push(username);
+    res.status(201).json({ message: 'Usuario creado', username });
+  } else {
+    res.status(400).json({ error: 'Nombre de usuario inválido o ya existe' });
+  }
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.listen(app.get('port'), () => console.log(`Server running on: http://localhost:${app.get('port')}`));
