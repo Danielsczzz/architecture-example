b8e70be33fa502149cacc7794b797b4e35304b99
dasczzz <danielscz.dsa@gmail.com>
2025-02-17 11:22:19 -0500

    feat: static files working

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express, { Express } from "express";
+import express, { Express, Request } from 'express'
+import cors from 'cors'
 
 const app: Express = express();
 
 app.set('port', 4000)
-app.use(express.static('./src/static'))
-app.use(express.json());
+app.use(cors<Request>())
+app.use(express.static('./src/view'))
+app.use(express.json())
 
 app.listen(app.get('port'), () => console.log(`Server running on: http://localhost:${app.get('port')}`))
